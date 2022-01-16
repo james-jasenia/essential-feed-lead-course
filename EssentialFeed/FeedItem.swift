6599cbb20911a6b6fc88be43a8eceaebc1682e7c
James <james.jasenia@gmail.com>
2022-01-16 17:12:50 +1100

    Added: `FeedItem` model.

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by james.jasenia on 16/1/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
