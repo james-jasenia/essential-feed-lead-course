4e2bbf537b9b9d7704d96e717c7b11fc337e0d5c
James <james.jasenia@gmail.com>
2022-01-17 07:25:46 +1100

    Directory: Moved `HTTPClient` and `RemoteFeedLoader` to production.

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -6,25 +6,7 @@
 //
 
 import XCTest
-
-class RemoteFeedLoader {
-    
-    let client: HTTPClient
-    let url: URL
-    
-    init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
+import EssentialFeed
 
 class RemoteFeedLoaderTests: XCTestCase {
     
@@ -57,5 +39,4 @@ class RemoteFeedLoaderTests: XCTestCase {
             requestedURL = url
         }
     }
-
 }
