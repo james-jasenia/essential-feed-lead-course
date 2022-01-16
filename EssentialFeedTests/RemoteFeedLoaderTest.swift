08bc77f61354abf49eec1515198c589105648cf4
James <james.jasenia@gmail.com>
2022-01-16 19:56:34 +1100

    Refactor: Tests are using the newly created `makeSUT()` method. Moved `HTTPClientSpy` within the test scope.

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -26,29 +26,36 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
-
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_HTTPClientInit_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
+        let (_, client) = makeSUT()
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestsDataFromURL() {
         let url = URL(string: "http://www.any-url.com/")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
     }
+    
+    //MARK: - Helpers
+    private func makeSUT(url: URL = URL(string: "http://www.any-url.com/")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut =  RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    //MARK: - Spies
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+
 }
