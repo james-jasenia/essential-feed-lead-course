ff60a6ebc4e3e454986194df23301462646a3f79
James <james.jasenia@gmail.com>
2022-01-16 18:57:00 +1100

    Added: Testing `RemoteFeedLoader` requests data from URL on `load()` using `HTTPClient` as a singleton.

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -9,16 +9,31 @@ import XCTest
 
 class RemoteFeedLoader {
     
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "http://www.any-url.com/")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_HTTPClientInit_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestsDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
