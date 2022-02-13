ee892eb2a4f0c23f84f29f2e5f52781c38b5d830
James <james.jasenia@gmail.com>
2022-02-13 14:14:45 +1100

    Add: `deleteCachedFeed()` upon save.

--- EssentialFeedTests/Feed API/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed API/CacheFeedUseCaseTests.swift
@@ -6,24 +6,58 @@
 //
 
 import XCTest
+import EssentialFeed
 
-class FeedCacheStore {
+class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class LocalFeedLoader {
-    let store: FeedCacheStore
+    let store: FeedStore
     
-    init(store: FeedCacheStore) {
+    init(store: FeedStore) {
         self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 class CacheFeedUseCaseTest: XCTestCase {
     
     func test_init_doesNotDeleteCacheUponCreation() {
-        let store = FeedCacheStore()
+        let store = FeedStore()
         _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [unqiueFeedItem(), unqiueFeedItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    //MARK: -- Helpers
+    private func unqiueFeedItem() -> FeedItem {
+        return FeedItem(
+            id: UUID(),
+            description: "any description",
+            location: "any location",
+            imageURL: anyURL()
+        )
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com/")!
+    }
+    
 }
