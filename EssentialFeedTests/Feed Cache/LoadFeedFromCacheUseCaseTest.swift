9e172302d6c4e955d91512654a571202d0ffd1a6
James <james.jasenia@gmail.com>
2022-02-16 19:26:21 +1100

    Refactor: Created `expect()` helper method.

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTest.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTest.swift
@@ -26,45 +26,28 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     func test_load_failsOnRetrievalError() {
         let (sut, store) = makeSUT()
         let retrievalError = anyNSError()
-        let exp = expectation(description: "Wait for completion")
         
-        var capturedError: Error?
-        sut.load() { result in
-            switch result {
-            case let .failure(receivedError):
-                capturedError = receivedError
-            default:
-                XCTFail("Expcted failure, got \(result) instead.")
-            }
-            exp.fulfill()
-        }
-        
-        store.completeRetrieval(with: retrievalError)
-        wait(for: [exp], timeout: 1.0)
-        
-        XCTAssertEqual(capturedError as NSError?, retrievalError)
+        expect(sut, toCompleteWith: .failure(retrievalError), when: {
+            store.completeRetrieval(with: retrievalError)
+        })
     }
     
     func test_load_deliversNoImagesOnEmptyCache() {
         let (sut, store) = makeSUT()
-        let exp = expectation(description: "Wait for completion")
-
-        var capturedImages: [FeedImage]?
-        sut.load() { result in
-            switch result {
-            case let .success(receivedImages):
-                capturedImages = receivedImages
-            default:
-                XCTFail("Expected success, got \(result) instead.")
-            }
-            exp.fulfill()
-        }
-
-        store.completeRetrievalWithEmptyCache()
-        wait(for: [exp], timeout: 1.0)
-
-        XCTAssertEqual(capturedImages, [])
+        expect(sut, toCompleteWith: .success([]), when: {
+            store.completeRetrievalWithEmptyCache()
+        })
     }
+    
+//    func test_load_deliversImagesOnLessThanSevenDaysOldCache() {
+//        let lessThanSevenDaysOldTimestamp = Date()
+//        let expectedItems = [makeAnyImage(), makeAnyImage()]
+//
+//        let (sut, store) = makeSUT()
+//        expect(sut, toCompleteWith: .success(expectedItems), when: {
+//            store.completeRetrieval(with: expectedItems, timestamp: lessThanSevenDaysOldTimestamp)
+//        })
+//    }
     
     // MARK: - Helpers
     
@@ -76,6 +59,46 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         
         return (sut, store)
+    }
+    
+    private func expect(_ sut: LocalFeedLoader, toCompleteWith expectedResult: LocalFeedLoader.LoadResult, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Wait for completion.")
+        
+        sut.load() { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedImages), .success(expectedImages)):
+                XCTAssertEqual(receivedImages, expectedImages, file: file, line: line)
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError as NSError, expectedError as NSError, file: file, line: line)
+            default:
+                XCTFail("Expected result \(expectedResult), got \(receivedResult) instead.", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+
+        action()
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private func makeAnyImage() -> FeedImage {
+        return makeImage(id: UUID(), description: "any description", location: "any location", imageURL: anyURL())
+    }
+    
+    private func makeImage(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> FeedImage {
+        
+        let image = FeedImage(
+            id: id,
+            description: description,
+            location: location,
+            url: imageURL
+        )
+        
+        return image
+    }
+    
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com/")!
     }
     
     private func anyNSError() -> NSError {
