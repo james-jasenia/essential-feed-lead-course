d1f46f6acc45a568746c91170c0e03e34b8a11ab
James <james.jasenia@gmail.com>
2022-02-18 20:40:51 +1100

    Added: `load` deletes cache and delivers empty items on exactly 7 days old cache.

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -36,7 +36,10 @@ public final class LocalFeedLoader {
             switch result {
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toDomainFeedImage()))
-            case .empty, .found:
+            case .found:
+                self.store.deleteCachedFeed() { _ in }
+                completion(.success([]))
+            case .empty:
                 completion(.success([]))
             case let .failure(error):
                 self.store.deleteCachedFeed() { _ in }
