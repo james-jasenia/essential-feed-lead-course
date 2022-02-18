2a2982bf81e4bcd89c139d0871f1ce87b031909c
James <james.jasenia@gmail.com>
2022-02-18 21:37:16 +1100

    Added: `ValidationFeedCacheUseCase` and testing the `init`.

---
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,29 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by james.jasenia on 18/2/22.
+//
+
+import XCTest
+import EssentialFeed
+
+class ValidateFeedCacheUseCaseTest: XCTestCase {
+    
+    func test_init_doesNotMessageTheStoreUponCreation() {
+        let (_, store) = makeSUT()
+        XCTAssertEqual(store.receievedMessages, [])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, store)
+    }
+}
