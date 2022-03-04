a5b512aa3f972c6883187425dacb64268e030a4d
James <james.jasenia@gmail.com>
2022-03-04 15:04:11 +1100

    Added: `CoreDataFeedStore.retrieve()` delivers empty on empty cache.

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -12,7 +12,7 @@ public final class CoreDataFeedStore: FeedStore {
     public init() {}
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        
+        completion(.empty)
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
--- EssentialFeedTests/Feed Cache/Frameworks/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/Frameworks/CoreDataFeedStoreTests.swift
@@ -6,10 +6,13 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
@@ -39,4 +42,11 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     func test_sideEffects_runSerially() {
         
     }
+    
+    //MARK: - Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+             let sut = CoreDataFeedStore()
+             trackForMemoryLeaks(sut, file: file, line: line)
+             return sut
+         }
 }
