48f8f0c68b2b3541f73aa54aeefb0c6b879e270d
James <james.jasenia@gmail.com>
2022-03-04 21:58:20 +1100

    Directory: Extract CoreData managed model classes into separate files.

---
+++ EssentialFeed/Feed Cache/Infrastructure/Core Data/ManagedFeedImage.swift
@@ -0,0 +1,35 @@
+//
+//  ManagedFeedItem.swift
+//  EssentialFeed
+//
+//  Created by james.jasenia on 4/3/22.
+//
+
+import CoreData
+
+@objc(ManagedFeedImage)
+internal class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
+}
+
+extension ManagedFeedImage {
+    
+    static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        return NSOrderedSet(array: localFeed.map { local in
+            let managed = ManagedFeedImage(context: context)
+            managed.id = local.id
+            managed.imageDescription = local.description
+            managed.location = local.location
+            managed.url = local.url
+            return managed
+        })
+    }
+    
+    var local: LocalFeedImage {
+        return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+    }
+}
