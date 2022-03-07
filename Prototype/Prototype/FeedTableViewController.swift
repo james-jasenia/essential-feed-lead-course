00a9e508de4a850d30e464c108b9820dacf2a2d4
James <james.jasenia@gmail.com>
2022-03-07 19:33:48 +1100

    Added: Prototype `.storyboard` with `UITableView` and UITableViewCell`.

---
+++ Prototype/Prototype/FeedTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FeedViewController.swift
+//  Prototype
+//
+//  Created by james.jasenia on 7/3/22.
+//
+
+import UIKit
+
+class FeedViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
