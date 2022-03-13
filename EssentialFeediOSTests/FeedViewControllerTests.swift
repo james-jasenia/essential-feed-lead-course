08488a157a6da76a499bf878d5263338d5a8e3a5
James <james.jasenia@gmail.com>
2022-03-14 08:25:29 +1100

    Added: Extract pull to refresh simulator to into a reusable extension on `UIRefreshControl`.

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -9,7 +9,7 @@ import XCTest
 import UIKit
 import EssentialFeed
 
-final class FeedViewController: UIViewController {
+final class FeedViewController: UITableViewController {
     
     private var loader: FeedLoader?
     
@@ -20,6 +20,14 @@ final class FeedViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        
+        load()
+    }
+    
+    @objc private func load() {
         loader?.load() { _ in }
     }
 }
@@ -40,6 +48,18 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 1)
     }
     
+    func test_pullToRefresh_loadsFeed() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        
+        sut.refreshControl?.simulatorPullToRefresh()
+        XCTAssertEqual(loader.loadCallCount, 2)
+        
+        sut.refreshControl?.simulatorPullToRefresh()
+        XCTAssertEqual(loader.loadCallCount, 3)
+    }
+    
     class LoaderSpy: FeedLoader {
         
         private(set) var loadCallCount: Int = 0
@@ -49,11 +69,19 @@ final class FeedViewControllerTests: XCTestCase {
         }
     }
     
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: UIViewController, loader: LoaderSpy) {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: UITableViewController, loader: LoaderSpy) {
         let loader = LoaderSpy()
         let sut = FeedViewController(loader: loader)
         trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
         return (sut, loader)
+    }
+}
+
+private extension UIRefreshControl {
+    func simulatorPullToRefresh() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach { (target as NSObject).perform(Selector($0))}
+        }
     }
 }
