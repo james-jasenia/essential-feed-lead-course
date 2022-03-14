6e157d78ce0eeaebde945fa2ec6dcd83e6fbeff0
James <james.jasenia@gmail.com>
2022-03-14 14:53:29 +1100

    Added: Feed image view retry button is visible on image url load error.

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -16,19 +16,19 @@ public protocol FeedImageDataLoader {
     typealias Result = Swift.Result<Data, Error>
     
     func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
- }
+}
 
- final public class FeedViewController: UITableViewController {
-     private var feedLoader: FeedLoader?
-     private var imageLoader: FeedImageDataLoader?
-     private var tableModel = [FeedImage]()
-     private var tasks = [IndexPath: FeedImageDataLoaderTask]()
-
-     public convenience init(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) {
-         self.init()
-         self.feedLoader = feedLoader
-         self.imageLoader = imageLoader
-     }
+final public class FeedViewController: UITableViewController {
+    private var feedLoader: FeedLoader?
+    private var imageLoader: FeedImageDataLoader?
+    private var tableModel = [FeedImage]()
+    private var tasks = [IndexPath: FeedImageDataLoaderTask]()
+    
+    public convenience init(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) {
+        self.init()
+        self.feedLoader = feedLoader
+        self.imageLoader = imageLoader
+    }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,34 +49,35 @@ public protocol FeedImageDataLoader {
             }
             
             self?.refreshControl?.endRefreshing()
-
+            
             
         }
     }
     
     public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-             return tableModel.count
-         }
-
-         public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-             let cellModel = tableModel[indexPath.row]
-             let cell = FeedImageCell()
-             cell.locationContainer.isHidden = (cellModel.location == nil)
-             cell.locationLabel.text = cellModel.location
-             cell.descriptionLabel.text = cellModel.description
-             cell.feedImageView.image = nil
-             cell.feedImageContainer.startShimmering()
-             tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
-                 if case let .success(data) = result {
-                     cell?.feedImageView.image = UIImage(data: data)
-                 }
-                 cell?.feedImageContainer.stopShimmering()
-             }
-             return cell
-         }
-     
-     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-         tasks[indexPath]?.cancel()
-         tasks[indexPath] = nil
-     }
+        return tableModel.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tableModel[indexPath.row]
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (cellModel.location == nil)
+        cell.locationLabel.text = cellModel.location
+        cell.descriptionLabel.text = cellModel.description
+        cell.feedImageView.image = nil
+        cell.feedImageRetryButton.isHidden = true
+        cell.feedImageContainer.startShimmering()
+        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
+            let data = try? result.get()
+            cell?.feedImageView.image = data.map(UIImage.init) ?? nil
+            cell?.feedImageRetryButton.isHidden = (data != nil)
+            cell?.feedImageContainer.stopShimmering()
+        }
+        return cell
+    }
+    
+    public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        tasks[indexPath]?.cancel()
+        tasks[indexPath] = nil
+    }
 }
