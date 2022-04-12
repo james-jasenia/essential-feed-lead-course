5b589d423bb15bb17139c9df7a330d14e31f6680
James Jasenia <jamesjasenia@Jamess-MacBook-Pro.local>
2022-04-12 18:14:38 +1000

    Test: `FeedPresenter` displays no error on `didStartLoadingFeed`.

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -8,7 +8,27 @@
 import XCTest
 
 final class FeedPresenter {
-    init(view: Any) {}
+    let errorView: FeedErrorView
+    
+    init(errorView: FeedErrorView) {
+        self.errorView = errorView
+    }
+    
+    func didStartLoadingFeed() {
+        errorView.display(.noError)
+    }
+}
+
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
+struct FeedErrorViewModel {
+    let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
 }
 
 class FeedPresenterTests: XCTestCase {
@@ -19,16 +39,32 @@ class FeedPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages.")
     }
     
+    func test_didStartLoadFeeding_displaysNoErrorMessage() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.messages, [.display(errorMessage: nil)])
+    }
+    
     private func makeSUT() -> (presenter: FeedPresenter, view: FeedViewSpy) {
         let view = FeedViewSpy()
-        let presenter = FeedPresenter(view: view)
+        let presenter = FeedPresenter(errorView: view)
         trackForMemoryLeaks(view)
         trackForMemoryLeaks(presenter)
         return (presenter, view)
     }
     
-    private class FeedViewSpy {
-        let messages = [Any]()
+    private class FeedViewSpy: FeedErrorView {
+        private(set) var messages = [Message]()
+        
+        enum Message: Equatable {
+            case display(errorMessage: String?)
+        }
+        
+        func display(_ viewModel: FeedErrorViewModel) {
+            messages.append(.display(errorMessage: viewModel.message))
+        }
     }
 }
 
